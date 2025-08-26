686bb742fce2135ba167ef80fed7cafff7869935
Gonzalo Martín Riquelme Ludwig <gonzariquelme66@gmail.com>
2025-08-26 19:18:51 -0300

    Reserve boking endpoint

---
+++ CleanBookings.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using CleanBookings.Api.Middleware;
+using CleanBookings.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanBookings.Api.Extensions;
+
+public static class ApplicationBuilderExtensions
+{
+    public static void ApplyMigrations(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+
+        using var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        dbContext.Database.Migrate();
+    }
+
+    public static void UseCustomExceptionHandler(this IApplicationBuilder app)
+    {
+        app.UseMiddleware<ExceptionHandlingMiddleware>();
+    }
+}
--- CleanBookings.Application/Apartments/SearchApartments/SearchApartmentsQueryHandler.cs
+++ CleanBookings.Application/Apartments/SearchApartments/SearchApartmentsQueryHandler.cs
@@ -44,7 +44,7 @@ internal sealed class SearchApartmentsQueryHandler
                 a.address_state AS State,
                 a.address_zip_code AS ZipCode,    
                 a.address_city AS City,
-                a.address_street AS Street,    
+                a.address_street AS Street    
             FROM apartments AS a
             WHERE NOT EXISTS
             (
